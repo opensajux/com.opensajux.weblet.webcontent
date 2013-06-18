442c193b7827b4c6b6cd5f49f5103065d07d8cba
smsajid <smsajid786@gmail.com>
2013-06-18 11:38:58 +0530

    initial configuration

---
+++ src/com/opensajux/weblet/webcontent/WebContentDisplay.java
@@ -0,0 +1,7 @@
+package com.opensajux.weblet.webcontent;
+
+import com.opensajux.weblet.GenericWeblet;
+
+public class WebContentDisplay extends GenericWeblet {
+
+}
---
+++ src/com/opensajux/weblet/webcontent/entity/WebContent.java
@@ -0,0 +1,11 @@
+package com.opensajux.weblet.webcontent.entity;
+
+import javax.jdo.annotations.PersistenceCapable;
+
+import com.opensajux.entity.Article;
+
+@PersistenceCapable
+public class WebContent extends Article {
+	private static final long serialVersionUID = 760666432802008133L;
+
+}
